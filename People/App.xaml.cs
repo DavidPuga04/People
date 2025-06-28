ca6744fef38c842c1f23e8b110675f4eea81406d
David Puga <david.puga@udla.edu.ec>
2025-06-28 16:20:49 -0500

    Personas deber

---
+++ People/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace People;
+
+public partial class App : Application
+{
+    public static PersonRepository PersonRepo { get; private set; }
+
+    public App(PersonRepository repo)
+    {
+        InitializeComponent();
+
+        PersonRepo = repo;
+
+        // Asume que tienes una MainPage creada para mostrar
+        MainPage = new MainPage();
+    }
+}
---
+++ People/MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace People;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        // Ruta a la base de datos
+        string dbPath = FileAccessHelper.GetLocalFilePath("people.db3");
+
+        // Registrar PersonRepository como singleton
+        builder.Services.AddSingleton<PersonRepository>(s => new PersonRepository(dbPath));
+
+        // Registrar App pasando el repositorio
+        builder.Services.AddSingleton<App>(s =>
+        {
+            var repo = s.GetRequiredService<PersonRepository>();
+            return new App(repo);
+        });
+
+        builder
+            .UseMauiApp<App>() // Esto ahora sí sabe usar App(repo)
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        return builder.Build();
+    }
+}
